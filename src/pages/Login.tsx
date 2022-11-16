68907971ca880570e41e93e43e25102116546227
Gagas Praharsa Bahar <gagaspbahar@gmail.com>
2022-11-16 14:36:37 +0700

    add react structure example

---
+++ src/pages/Login.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+function Login() {
+  return (
+    <>
+      <Navbar />
+      <div>Ini Login</div>
+    </>
+  )
+}
+
+export default Login
