ec0088706c634a7437ead4dec93f9f860854bc5b
lyorafelicya <13520073@std.stei.itb.ac.id>
2022-11-21 22:58:58 +0700

    add font dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme  } from '@chakra-ui/react'
 import './index.css'
+
+const theme = extendTheme({
+  fonts: {
+    body: `'Poppins', sans-serif`,
+  },
+})
+
+export default theme
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Router>
     <React.StrictMode>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <App />
       </ChakraProvider>
     </React.StrictMode>
