4e2ce92a036e43acd1718129de93a8c7f844c1e0
lyorafelicya <13520073@std.stei.itb.ac.id>
2022-11-21 23:07:20 +0700

    feat: add login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,25 +1,86 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-
+import premiumLogo from "../assets/premium-logo.svg";
 import {
-  Input,
-  Button,
-  ButtonGroup,
+  Flex,
+  Box,
   FormControl,
   FormLabel,
-  FormErrorMessage,
-  FormHelperText,
-} from '@chakra-ui/react'
+  Input,
+  InputGroup,
+  InputRightElement,
+  Stack,
+  Button,
+  Text,
+  useColorModeValue,
+  Link,
+  Image
+} from '@chakra-ui/react';
+
+import { useState } from 'react';
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+
 
 function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+  const handleShowClick = () => setShowPassword(!showPassword);
+
   return (
     <>
-      <div>Ini Login</div>
-      <FormControl isRequired>
-        <FormLabel>First name</FormLabel>
-          <Input placeholder='First name' />
-      </FormControl>
-      <Button colorScheme='blue'>Button</Button>
+      <Flex
+        minH={'100vh'}
+        align={'center'}
+        justify={'center'}
+        textColor={'white'}
+        bg={useColorModeValue('#121212', 'gray.800')}>
+          
+      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={8} px={6}>
+        <Stack align={'center'}>
+          <Image src={premiumLogo}  alt="logo" mb={8}/>
+          <Text fontSize={'lg'} color={'white'}>
+            To continue, login to Binotify.
+          </Text>
+        </Stack>
+
+        <Box>
+          <Stack spacing={4}>
+            <FormControl id="username" isRequired>
+              <FormLabel>Username</FormLabel>
+              <Input type="text" bg="#212121" placeholder="Enter your username" border={"none"} />
+            </FormControl>
+
+            <FormControl id="password" isRequired>
+              <FormLabel>Password</FormLabel>
+              <InputGroup>
+                <Input type={showPassword ? "text" : "password"} bg="#212121" placeholder="Enter your password" border={"none"}/>
+                <InputRightElement h={'full'}>
+                  <Button
+                    variant={'ghost'}
+                    onClick={handleShowClick}>
+                    {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+            </FormControl>
+
+            <Stack spacing={10} pt={2}>
+              <Button
+                borderRadius={"22px"}
+                size="lg"
+                bg={'#1DB954'}
+                color={'#121212'}
+                _hover={{ bg: '#169844'}}>
+                Login
+              </Button>
+            </Stack>
+
+            <Stack pt={6}>
+              <Text align={'center'} textColor="#939393">
+                Didn't have an account? <Link color={'#1DB954'}>Sign Up</Link>
+              </Text>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
 
     </>
   )
