dd16cff59b82b6ec22aa1732754cb526a4a19a92
lyorafelicya <13520073@std.stei.itb.ac.id>
2022-11-21 13:51:33 +0700

    add chakra provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Router>
     <React.StrictMode>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </React.StrictMode>
   </Router>
 )
