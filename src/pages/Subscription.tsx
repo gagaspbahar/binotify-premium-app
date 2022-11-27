b99cfff1ce816bcd814b53d8bc9ca198db1d2f4a
Claudia <88203931+clauculus@users.noreply.github.com>
2022-11-27 14:40:00 +0700

    feat: acc reject subscription

--- src/pages/Subscription.tsx
+++ src/pages/Subscription.tsx
@@ -11,16 +11,52 @@ import {
   Tr,
   Th,
   Td,
-  TableCaption,
   TableContainer,
+  Select,
+  Button,
+  ButtonGroup,
 } from "@chakra-ui/react";
 
+import { useState } from "react";
+
+import { colors } from "../theme";
+
+interface Users {
+  no: number;
+  username: string;
+  email: string;
+  status: string;
+}
+
+const initialUsers: Users[] = [
+  {
+    no: 1,
+    username: "gagasgede",
+    email: "gagasgede@binotify.com",
+    status: "PENDING",
+  },
+  {
+    no: 2,
+    username: "clauculus",
+    email: "clauculus@binotify.com",
+    status: "ACCEPTED",
+  },
+  {
+    no: 3,
+    username: "lyoraf",
+    email: "lyo@binotify.com",
+    status: "PENDING",
+  },
+];
+
 function Subscription() {
+  const [users, setUsers] = useState(initialUsers);
+
   return (
     <>
       <Navbar children={undefined} />
       <Box minH="100vh" bg="#212121" textColor="white" ml={{ base: 0, md: 60 }}>
-        <Text fontSize="4xl" fontWeight="bold" textAlign="left" ml="10" pb="10">
+        <Text fontSize="4xl" fontWeight="bold" textAlign="center" pb="10">
           Subscription Requests
         </Text>
         <TableContainer px="10">
@@ -34,48 +70,42 @@ function Subscription() {
               </Tr>
             </Thead>
             <Tbody>
-              <Tr>
-                <Td>1</Td>
-                <Td>gagasbestiegede</Td>
-                <Td>gagasgede@binotify.com</Td>
-                <Td>ditolak</Td>
-              </Tr>
-              <Tr>
-                <Td>2</Td>
-                <Td>gagasbestiegede</Td>
-                <Td>gagasgede@binotify.com</Td>
-                <Td>ditolak</Td>
-              </Tr>
-              <Tr>
-                <Td>3</Td>
-                <Td>gagasbestiegede</Td>
-                <Td>gagasgede@binotify.com</Td>
-                <Td>ditolak</Td>
-              </Tr>
-              <Tr>
-                <Td>4</Td>
-                <Td>gagasbestiegede</Td>
-                <Td>gagasgede@binotify.com</Td>
-                <Td>ditolak</Td>
-              </Tr>
-              <Tr>
-                <Td>5</Td>
-                <Td>gagasbestiegede</Td>
-                <Td>gagasgede@binotify.com</Td>
-                <Td>ditolak</Td>
-              </Tr>
-              <Tr>
-                <Td>6</Td>
-                <Td>gagasbestiegede</Td>
-                <Td>gagasgede@binotify.com</Td>
-                <Td>ditolak</Td>
-              </Tr>
-              <Tr>
-                <Td>7</Td>
-                <Td>gagasbestiegede</Td>
-                <Td>gagasgede@binotify.com</Td>
-                <Td>ditolak</Td>
-              </Tr>
+              {users.map((user) => {
+                return (
+                  <Tr>
+                    <Td>{user.no}</Td>
+                    <Td>{user.username}</Td>
+                    <Td>{user.email}</Td>
+                    <Td>
+                      {user.status === "PENDING" ? (
+                        <ButtonGroup gap="2">
+                          <Button
+                            bg={colors.primaryColor}
+                            _hover={{
+                              bg: colors.white,
+                              color: "black",
+                            }}
+                          >
+                            Accept
+                          </Button>
+
+                          <Button
+                            bg={colors.red}
+                            _hover={{
+                              bg: colors.white,
+                              color: "black",
+                            }}
+                          >
+                            Reject
+                          </Button>
+                        </ButtonGroup>
+                      ) : (
+                        user.status
+                      )}
+                    </Td>
+                  </Tr>
+                );
+              })}
             </Tbody>
           </Table>
         </TableContainer>
