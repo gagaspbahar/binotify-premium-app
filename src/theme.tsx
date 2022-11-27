35e902c676000c2f302e737e9fe5dada274a526b
Claudia <88203931+clauculus@users.noreply.github.com>
2022-11-27 15:06:05 +0700

    feat: add theme

---
+++ src/theme.tsx
@@ -0,0 +1,10 @@
+const colors = {
+  primaryColor: "#1DB954",
+  red: "#FF0052",
+  white: "#FFFFFF",
+  afterHover: "#282828",
+  navbarColor: "#121212",
+  bgColor: "#212121",
+};
+
+export { colors };
